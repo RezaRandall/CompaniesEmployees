84851924a173a11c686f27a25baac92a75b9b478
Dwi Reza Irawan <dwirezairawan07@gmail.com>
2023-03-03 14:12:37 +0700

    Add new project CompaniesAndEmployees

---
+++ DapperASPNetCore/Dto/CompanyForUpdateDto.cs
@@ -0,0 +1,8 @@
+﻿namespace DapperASPNetCore.Dto;
+
+public class CompanyForUpdateDto
+{
+    public string? Name { get; set; }
+    public string? Address { get; set; }
+    public string? Country { get; set; }
+}
